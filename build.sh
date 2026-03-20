bf84ce57b6e76e99722ca5eb065053aa250e5f34
aitorivera11 <103530568+aitorivera11@users.noreply.github.com>
2026-03-20 08:34:52 +0100

    Remove echo and find commands from build.sh

--- build.sh
+++ build.sh
@@ -2,5 +2,3 @@
 set -euo pipefail
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput --clear
-echo "=== Contingut de staticfiles_build/static/ ==="
-find staticfiles_build/static/ -type f | head -40
