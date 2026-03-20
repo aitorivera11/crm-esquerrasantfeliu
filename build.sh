e4f81266c274152fcfb76fdae191f5a8023d7476
aitorivera11 <103530568+aitorivera11@users.noreply.github.com>
2026-03-20 09:05:26 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,3 +2,4 @@
 set -euo pipefail
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput --clear
+echo "BUILD DONE: $(find staticfiles_build/static -type f | wc -l) ficheros estàtics"
