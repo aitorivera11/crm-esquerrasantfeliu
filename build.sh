34a8df3956ba523b6399804c9945a03e95550f12
aitorivera11 <103530568+aitorivera11@users.noreply.github.com>
2026-03-20 08:32:59 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
-
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput --clear
+echo "=== Contingut de staticfiles_build/static/ ==="
+find staticfiles_build/static/ -type f | head -40
