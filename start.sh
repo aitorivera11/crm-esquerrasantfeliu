acf02fc8ec322e4992e39fdfb1eed4ce8f08b9db
aitorivera11 <103530568+aitorivera11@users.noreply.github.com>
2026-03-26 19:49:41 +0100

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+python manage.py migrate --noinput
+python manage.py collectstatic --noinput --clear
+
+exec gunicorn config.wsgi:application --bind 0.0.0.0:8000
