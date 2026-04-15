dc48f103675c6dcd624b48463c741d0ac094df86
aitorivera11 <103530568+aitorivera11@users.noreply.github.com>
2026-04-15 12:50:12 +0200

    Add WhiteNoise configuration to WSGI
    Configure WhiteNoise to serve media files from the specified directory.

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,8 +1,12 @@
 import os
-
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
-app = get_wsgi_application()
-application = app
+application = get_wsgi_application()
+
+# Configura WhiteNoise per servir la carpeta media
+# 'root' ha de coincidir amb el teu MEDIA_ROOT (/app/media)
+# 'prefix' ha de coincidir amb el teu MEDIA_URL (/media/)
+application = WhiteNoise(application, root='/app/media', prefix='/media/')
